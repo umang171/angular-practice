f8d5720ff6a731d0c62cf2313c51c61b1b127a6b
Umang Gohel <gohelumang12@gmail.com>
2023-05-24 15:11:16 +0530

    onchanges hook in angular life cycle

--- src/app/passenger-dashboard/components/passenger-details/passsenger-details.component.ts
+++ src/app/passenger-dashboard/components/passenger-details/passsenger-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output,OnChanges, SimpleChanges} from '@angular/core';
 import { Passenger } from '../../models/passenger.interfase';
 
 @Component({
@@ -6,24 +6,38 @@ import { Passenger } from '../../models/passenger.interfase';
     styleUrls:['passenger-details.component.css'],
     template:`
         <li class="border border-1 mt-1">
-        <span class="bg-red rounded-circle px-2" [ngClass]="{'checked-in':!detail.status}"></span> {{detail.id}} 
+        <span class="bg-red rounded-circle px-2" [ngClass]="{'checked-in':detail.status}"></span> {{detail.id}} 
         <div *ngIf="isEdit"><input [value]="detail.name" (input)=onNameChange(user.value) #user></div>
         <div *ngIf="!isEdit">{{detail.name}}</div>
         <div>{{detail.checkedIn|date:"y MMMM d"}}</div>
         <div>Children-{{detail.children?.length||0}}</div>
         <button (click)="toggleEdit()" class="btn btn-info">{{!isEdit?"Edit":"Done"}}</button>
+        <button (click)="removePas()" class="btn btn-info ms-2">Delete</button>
         </li>
     `
 })
-export class PassengerDetailsComponent{
+export class PassengerDetailsComponent implements OnChanges{
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log(changes);
+    }
     @Input()
     detail!: Passenger;
     isEdit:boolean=false;
+
+    @Output()
+    editPass:EventEmitter<Passenger>=new EventEmitter();
+    @Output()
+    removePass:EventEmitter<Passenger>=new EventEmitter();
 
     onNameChange(name:string){
         this.detail.name=name;
     }
     toggleEdit(){
+        if(this.isEdit)
+            this.editPass.emit(this.detail);
         this.isEdit=!this.isEdit;
+    }
+    removePas(){
+        this.removePass.emit(this.detail);
     }
 }
--- src/app/passenger-dashboard/container/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/container/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,8 +7,11 @@ import { Passenger } from "../../models/passenger.interfase";
     <div>
     <passenger-count [items]="passengers"></passenger-count>
     <h1>Passenger Details</h1>
+    <div *ngFor="let passenger of passengers">
+      {{passenger.name}}
+    </div>
     <ul>
-    <passenger-details *ngFor="let passenger of passengers" [detail]="passenger"></passenger-details>
+    <passenger-details *ngFor="let passenger of passengers" [detail]="passenger" (removePass)="removePass($event)" (editPass)="editPass($event)"></passenger-details>
     </ul>  
     </div>
     `
@@ -45,7 +48,20 @@ export class PassengerDashboardComponent implements OnInit{
               checkedIn:17102020,
               children:null
             }
-          ];  
-    
+          ];           
+    }
+    editPass(passengerEdit:Passenger){
+      this.passengers=this.passengers.map((passenger:Passenger)=>{
+        if(passenger.id===passengerEdit.id)
+        {
+          passenger=Object.assign({},passenger,passengerEdit);
+        }
+        return passenger;
+      });
+    }
+    removePass(passenger:Passenger){
+      this.passengers=this.passengers.filter((passengr:Passenger)=>{
+        return passengr.id!==passenger.id;
+      })
     }
 }
